7a051da1a2102d2fb5f61ae718ae7162be69f20e
Fa1tumn <ts01088978343@gmail.com>
2025-04-25 18:11:31 +0900

    ✨ feat(calendar): modularize cookie handling and link extraction
    - 【learnus_getcookie.py】:
      - refactor to encapsulate cookie retrieval into `get_cookies` function
      - adds main block to allow execution of cookie retrieval as a script
    - 【learnus_calendar.py】:
      - removes cookie loading from file, expects cookies as a parameter
      - introduces `get_links` function to fetch and parse links, using cookies

--- learnus_calendar.py
+++ learnus_calendar.py
@@ -3,32 +3,25 @@ import requests
 import json
 from bs4 import BeautifulSoup
 
-# Load cookies from a JSON file
-with open('cookies.json', 'r') as file:
-    cookies_list = json.load(file)
+def get_links(cookies):
+    # URL of the website
+    url = "https://ys.learnus.org/calendar/view.php?view=upcoming"
 
-# Convert cookies list to a dictionary
-cookies = {cookie['name']: cookie['value'] for cookie in cookies_list}
+    # Send a GET request with cookies
+    response = requests.get(url, cookies=cookies)
 
-# URL of the website
-url = "https://ys.learnus.org/calendar/view.php?view=upcoming"
+    # Check if the request was successful
+    if response.status_code == 200:
+        print("HTML content fetched successfully!")
+        # Write the HTML content to a file
+        html_content = response.text
+            # 使用正则表达式提取注释中的 <a> 标签
+        pattern = r'<!--\s*<a href="(.*?)">.*?</a>\s*-->'
+        links = re.findall(pattern, html_content)
 
-# Send a GET request with cookies
-response = requests.get(url, cookies=cookies)
-
-# Check if the request was successful
-if response.status_code == 200:
-    print("HTML content fetched successfully!")
-    # Write the HTML content to a file
-    html_content = response.text
-else:
-    print(f"Failed to fetch HTML. Status code: {response.status_code}")
+        return links
+    else:
+        print(f"Failed to fetch HTML. Status code: {response.status_code}")
+        return None
 
 
-# 使用正则表达式提取注释中的 <a> 标签
-pattern = r'<!--\s*<a href="(.*?)">.*?</a>\s*-->'
-links = re.findall(pattern, html_content)
-
-# 打印所有链接
-for link in links:
-    print("link", link)
